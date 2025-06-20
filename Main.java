26dd21e6ffc9dde9f2425ac280b1ea1daa7cd0ed
J0nathanjc082 <6300532@clases.edu.sv>
2025-06-19 21:42:54 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginForm loginForm = new LoginForm();
+            loginForm.setVisible(true);
+        });
+    }
+}
